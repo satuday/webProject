07e34210fcd5da7cf49977571d4df9c239448f0d
satuday <felix_zhang@yahoo.com>
2013-12-07 15:04:21 -0500

    a

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@
 
 */
 
-
+function select(id) {
+    $("ul.tabbed > li").each(function (index) {
+        if (this.attributes['id'].value == id)
+            this.attributes["class"].value = "current-tab";
+        else
+            this.attributes["class"].value = "";
+    })
+}
+function selectVideo(url) {
+    $("iframe")[0].attributes["src"].value = url;
+ 
+}
 
 
 
